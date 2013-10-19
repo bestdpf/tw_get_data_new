3efb5dda1119acaecb892b076aa0ce9555d13bad
pengfeituan-f18 <pengfeituan@gmail.com>
2013-10-19 14:14:52 +0800

    0.0.9

--- do.sh
+++ do.sh
@@ -3,11 +3,7 @@ while [ true ]
 do
 	while read line
 	do
-	python ./get_data_multi.py $line
-	done < "fail.txt"
-	while read line
-	do
 	echo $line 
-	#python ./get_data_multi.py $line
+	python ./get_data_multi.py $line
 	done < "last_succ.txt"
 done
